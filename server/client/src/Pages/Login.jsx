f2272a3da5f3b4b68584542d2565fff06bce3bc0
At. <130650439+atulya-srivastava@users.noreply.github.com>
2024-09-01 12:06:00 +0530

    Clubbed Multiple States

--- server/client/src/Pages/Login.jsx
+++ server/client/src/Pages/Login.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail, password, setPassword] = useState('');
     const navigate = useNavigate();
 
     const handleLogin = (e) => {
